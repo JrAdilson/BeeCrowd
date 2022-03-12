4475f70c167b147c2ea43f51d69b70db43544944
JrAdilson <imjrgonc@gmail.com>
2022-03-11 23:33:55 -0300

    std pt3

---
+++ C/Basic.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int A,B,X;
+    scanf("%i",&A);
+    scanf("%i",&B);
+    X=A+B;
+    printf("X = %i",X);
+    return 0;
+}
---
+++ Java/Basic.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int A = s.nextInt();
+        int B = s.nextInt();
+        int X = A+B;
+        System.out.println("X = "+X);
+    }
+}
